68c14937d6cb76c3810db0b9d0db7374cc4ea332
tejahree <41065443+tejahree@users.noreply.github.com>
2020-01-03 09:45:41 +0530

    Update Anagram.java

--- src/com/bridgelabz/fellowshipprogram/alogorithm/Anagram.java
+++ src/com/bridgelabz/fellowshipprogram/alogorithm/Anagram.java
@@ -1,30 +1,15 @@
 package com.bridgelabz.fellowshipprogram.alogorithm;
 
-import java.util.Arrays;
 import java.util.Scanner;
+
+import com.bridgelabz.fellowshipprogram.datastructure.Utility;
 
 public class Anagram {
 	
 	
-	static boolean anagramOrNot(char[] str,char[] str1) {
-		int n1=str.length;
-		int n2=str1.length;
-		
-		if(n1!=n2)
 	
-			return false;
-		Arrays.sort(str);
-		Arrays.sort(str1);
-		for(int i=0;i<n1;i++) 
-			if(str[i]!=str1[i])
-				return false;
-		
-		
-		
-		return true;
-	}
 public static void main(String args[]) {
-	
+	Utility utility=new Utility();
 	
 	Scanner sc= new Scanner(System.in);
 	
@@ -35,7 +20,7 @@ public static void main(String args[]) {
 	
 	System.out.println("Enter the String 2: ");
 	char str1[]= sc.next().toCharArray();
-if(anagramOrNot(str,str1)) {
+if(utility.anagramOrNot(str,str1)) {
 	System.out.println("both string are Anagram");
 }else {
 	System.out.println("both string are not anagram");
