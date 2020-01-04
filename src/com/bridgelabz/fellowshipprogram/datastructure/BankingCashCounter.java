81be3a78bf14e35cf1ccf8dcec08a038517eae31
tejahree <41065443+tejahree@users.noreply.github.com>
2020-01-04 18:52:24 +0530

    Update BankingCashCounter.java

--- src/com/bridgelabz/fellowshipprogram/datastructure/BankingCashCounter.java
+++ src/com/bridgelabz/fellowshipprogram/datastructure/BankingCashCounter.java
@@ -2,11 +2,9 @@ package com.bridgelabz.fellowshipprogram.datastructure;
 
 import java.util.Scanner;
 
-
-
-
 public class BankingCashCounter {
-static class QueueImplement{
+
+	static class QueueImplement{
 		
 		static int[] queue=new int[10];
 		static	int f=-1,r=-1;
@@ -28,9 +26,9 @@ static class QueueImplement{
 						capacity--;
 						//System.out.println(f);
 						//System.out.println(r); 
-						System.out.println("value add sussfully"+"  "
+						System.out.println("Token Number is added"+"  "
 								+ ""+ value+"\n");
-						System.out.println("THIS IS QUEUE\n");
+						System.out.println("List of TokenNUmber\n");
 						print();
 						}
 					
@@ -41,8 +39,8 @@ static class QueueImplement{
 						capacity--;
 					//	System.out.println(f);
 					//	System.out.println(r);
-						System.out.println("value add sussfully"+"  "+ value+"\n");
-						System.out.println("THIS IS QUEUE\n");
+						System.out.println("Token Number "+"  "+ value+"is added\n");
+						System.out.println("List of TokenNUmber\n");
 						print();
 					}
 					}
@@ -55,7 +53,7 @@ static class QueueImplement{
 					System.out.println("Queue is empty\n");
 				}else {
 					int element=queue[f];
-					System.out.println("value is been deleted "+ element);
+					System.out.println("Next TokenNumber ");
 					f++;
 					capacity++;
 					print();
@@ -69,6 +67,17 @@ static class QueueImplement{
 				else
 				return false;
 			}
+		static void addAtLast(int value) {
+			r++;
+			queue[r]=value;
+			
+			capacity--;
+		//	System.out.println(f);
+		//	System.out.println(r);
+			System.out.println("TokenNUmber"+"  "+ value+"is added\n");
+			System.out.println("List of TokenNumber\n");
+			print();
+		}
 		static void print() {
 			for(int i=f;i<=r;i++) {
 				System.out.println(queue[i]+ "  ");
@@ -76,64 +85,63 @@ static class QueueImplement{
 		}
 		
 	}	
-		/*static int addlogic(int value) {
-			
-			
-			enqueue(value);
-			
-			return capacity;
-			
-		}
-		static void dequelogic(){
-			dequeue();
-			print();
-		}*/
-	public static void main(String[] args) {
-		QueueImplement q= new QueueImplement();
-        int balance=50000;int cash=0;
-		Scanner sc=new Scanner(System.in);
-		int c=10;
 
-		while(c!=0) {
+	public static void main(String[] args) {
+		QueueImplement queue= new QueueImplement();
+        int balance=50000;int tokenNumber=500;
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the Number Of Customer in Queue\n");
+		int numberofcustomer= scanner.nextInt();
+		System.out.println("CASH COUNTER\n");
+		//System.out.println("1.Add into Queue\n");
+		int amount;
+		for(int i=1;i<=numberofcustomer;i++) {
 			
-	System.out.println("BANKING CASH COUNTER\n");
-	System.out.println("1.ADD IN QUEUE\n");
-	System.out.println("2.DEQUEUE FORM QUEUE\n");
-	
-	int option=sc.nextInt();
-	
-	switch(option) {
-	case 1:System.out.println("Enter acc number\n");
-			int value=sc.nextInt();			
-			q.enqueue(value);
-			System.out.println("1.deposit\n");
-			System.out.println("2.widthdraw\n");
-			System.out.println("3.exit\n");
-			int opt=sc.nextInt();
-			switch(opt) {
-			case 1:System.out.println("enter the amount\n");
-					cash=sc.nextInt();
-					balance=balance+cash;
-					System.out.println("Balance is: "+balance);
-					break;
-					
-			case 2:if(balance==0)
-					System.out.println("zero balance\n");
-					else {
-							System.out.println("enter the amount\n");
-							cash=sc.nextInt();
-							balance=balance-cash;
-							System.out.println("Balance is: "+balance);
-						}
+			queue.enqueue(tokenNumber++);
+			
+				}
+		while(numberofcustomer!=0) {
+		
+		
+			System.out.println("CASH COUNTER\n");
+			System.out.println("1.Diposit\n");
+			System.out.println("2.Widthdraw\n");
+			System.out.println("Enter the option\n");
+			int option=scanner.nextInt();
+			switch(option) {
+			case 1: System.out.println("Enter Amount for Diposit\n");
+				amount=scanner.nextInt();
+				balance=balance+amount;
+				System.out.println("BALANCE:=  "+balance);
+				numberofcustomer--;
+				queue.dequeue();
+				break;
+				
+			case 2:System.out.println("Enter Amount for Widthdraw\n");
+			amount=scanner.nextInt();
+				if(amount<=balance) {
+					balance=balance-amount;
+					System.out.println("BALANCE:=  "+balance);
+					numberofcustomer--;
+					queue.dequeue();
 					}
-			c--;
-			break;
-	case 2:q.dequeue();	
-			c++;
-			break;
-	}
+				else {
+					System.out.println("That much amount is not present");
+					
+						if((numberofcustomer==1) ||(queue.isEmpty())) {
+							System.out.println("Sorry cash is not available\n");
+					}else {
+						queue.addAtLast(tokenNumber);
+						queue.dequeue();
+					}
+				}
+				break;
+				
+				default:System.out.println("please enter correct option\n");
+		}
 	
-	}
+		
+}
 }
 
 }
