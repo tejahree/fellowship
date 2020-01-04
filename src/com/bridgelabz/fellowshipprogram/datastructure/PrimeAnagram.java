9c86688f5e841e1f7cc41a828a852c7e292d32a5
tejahree <41065443+tejahree@users.noreply.github.com>
2020-01-04 18:54:06 +0530

    Update PrimeAnagram.java

--- src/com/bridgelabz/fellowshipprogram/datastructure/PrimeAnagram.java
+++ src/com/bridgelabz/fellowshipprogram/datastructure/PrimeAnagram.java
@@ -1,77 +1,94 @@
 package com.bridgelabz.fellowshipprogram.datastructure;
 
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class PrimeAnagram {
+	
 	public static void main(String args[]) {
-		boolean result;
-		int i=0;
-		int[] arr= new int[168];
-		System.out.println("So prime number are: ");
-	for(int x=2;x<=1000;x++) {
-		result=primeornot(x);
-		if(result==true) {
 		
-			arr[i]=x;
-		i++;
-		}
+			int[] primeArray= new int[168];
+			/*
+			 * this Utility.returnPrimeNumberArray() function return
+			 * primenumber from range 0 to 1000 in primeArray
+			 */
+			primeArray=Utility.returnPrimeNumberArray();
+			/*
+			 * set is create to store an anagram number from the primeArray
+			 */
+			Set<Integer> passSet=new HashSet<Integer>();
+			/*
+			 * this anagram function return set of anagramNumber 
+			 */
+			passSet=anagram(primeArray);
+			int size=passSet.size();
+			int[] setToArray = new int[size]; 
+			/*
+			 * this for loop convert the set into array and store it into SettoArray
+			 */
+			int k = 0; 
+			for (int x : passSet) {
+				setToArray[k++] = x; 
+			}
+			
+			/*
+			 * this sort the anagram array
+			 */
+			int[] sortedArray=new int[size];
+			sortedArray=Utility.bubblesort(setToArray);
+			int[][] array2d=new int[8][15];
+			int f=0;
+			/*
+			 * this for loops covert the single array into 2D array 
+			 */
+			for(int m=0;m<8;m++) {
+				for(int n=0;n<15;n++) {
 		
-	}
-	for(int j=0;j<168;j++) {
-		System.out.println(arr[j]);
-	}
-int[] primeanagram = anagram(arr);
-	// primeanagram	=;
-	 
-	System.out.println("this is angram array\n");
-	for(int l=0;l<100;l++) {
-		//if(primeanagram[l]!=primeanagram[l+1])
-		System.out.println(primeanagram[l]);
-	}
-	}
-	
-	
-	
-	static int[] anagram(int[] arr) {
-		int[] anagram= new int[100];
-		int k=0;
-		for(int i=0;i<168;i++  ) {
-		for(int j=0;j<168;j++){
-			if(anagramnumber(arr[i],arr[j])) {
-				anagram[k]=arr[i];
-				k++;
-				anagram[k]=arr[j];
-				k++;
+					if((f<size)) {
+						array2d[m][n]=sortedArray[f];
+						f++;
+					}
+					}
+
+			}
+			/*
+			 * this print the 2d array which contain both prime and anagram number
+			 */
+			System.out.println("This is the AnagramNumber 2D Array: \n");
+			for(int m=0;m<8;m++) {
+				for(int n=0;n<15;n++) {
+					String p=String.format("%3d", array2d[m][n]);
+					System.out.print(p+"  ");
+					}
+				System.out.println();
 			}
 		}
-		}
-		return anagram;
-	}
-	static boolean anagramnumber(int e1,int e2) {
-		boolean status =true;
-		int[] arr=Utility.intintoarray(e1);
-		int[] arr2=Utility.intintoarray(e2);
-		Utility u= new Utility();
-		int[] sortarr1=Utility.bubblesort(arr);
-		int[] sortarr2=Utility.bubblesort(arr2);
-		for(int i=0;i<10;i++) {
-			if((sortarr1[i]==sortarr2[i]) && (status==true)) 
-				status=true;
-			else 
-				status=false;
-		}
-		return status;
-		
-	}
+	/**
+	 * this method return set of prime number which are anagram also 
+	 * @parameter int array  
+	 *@return return set of anagram number from prime number
+	 */
+	
+			static Set anagram(int[] arr) {
+				Set<Integer> set= new HashSet<Integer>();
+
+					
+					for(int i=0;i<168;i++  ) {
+						for(int j=0;j<168;j++){
+							if(i!=j) {
+							if(Utility.anagramnumber(arr[i],arr[j])) {
+								
+									set.add(arr[i]);
+									set.add(arr[j]);	
+								}
+							}
+							}
+						}
+					
+					return set;
+			}
 	
 	
-	
-	static boolean primeornot(int x) {
-		int i=1;
-		do {
-			i++;
-		}while(x%i!=0);
-		if(x==i) { 
-			return true;
-		}else {return false;}
-	
-	}
+
 }
